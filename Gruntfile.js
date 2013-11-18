04997eca4014b486c60f2aec0eecf5b7cc56b497
John Ferris <john@pixel-whip.com>
2013-11-18 20:10:19 +1030

    Change livereload port so it doesn't conflict with theapp.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 // Generated on 2013-11-14 using generator-webapp 0.4.1
 'use strict';
-var LIVERELOAD_PORT = 35729;
+var LIVERELOAD_PORT = 35728;
 var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
 var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
