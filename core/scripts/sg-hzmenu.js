85c8399d1b71e6dadf1f639e0ff515e9f7af85a1
John Ferris <john@pixel-whip.com>
2014-01-28 09:40:07 -0700

    Fix body click event bug.

--- core/scripts/sg-hzmenu.js
+++ core/scripts/sg-hzmenu.js
@@ -3,50 +3,62 @@
 var sgHorizontalMenu = (function() {
 
   var $listItems = document.querySelectorAll('#sg-hzmenu > ul > li'),
-    $body = document.querySelector('body'),
+    $body = document.getElementsByTagName('body')[0],
     current = -1;
 
   function init() {
-    document.addEventListener('click', function(e) {
-      if ( e.target.matchesSelector('#sg-hzmenu > ul > li > a') ) {
+    [].forEach.call(
+      document.querySelectorAll('#sg-hzmenu > ul > li > a'), function(el) {
+      el.addEventListener('click', function(e) {
         open(e);
-      }
-    }, false);
-    //$listItems.on( 'click', function( event ) { event.stopPropagation(); } );
+        e.stopPropagation();
+      })
+    });
   }
 
-  function open( event ) {
-
+  function open(event) {
     // Remove the open class from any that are set.
     if( current !== -1 ) {
       $listItems[current].classList.remove( 'sg-hzopen' );
     }
 
+    // Determine the index of the target list item.
     var $item = event.target.parentNode,
       el = $item,
-      idx = 0;
+      idx = 0,
+      $event = event;
 
     while (el.previousElementSibling) {
       idx++;
       el = el.previousElementSibling;
     }
 
+    // If clicking the current item, close it.
     if( current === idx ) {
       $item.classList.remove( 'sg-hzopen' );
       current = -1;
     }
+    // else, open it.
     else {
       $item.classList.add( 'sg-hzopen' );
       current = idx;
+      $body.removeEventListener('click', close, false);
+      $body.addEventListener('click', function(e) {
+        console.log(e.target);
+        close();
+      }, false);
     }
 
     return false;
 
   }
 
-  function close( event ) {
-    $listItems[current].classList.remove( 'sg-hzopen' );
+  function close() {
+    if (current !== -1) {
+      $listItems[current].classList.remove('sg-hzopen');
+    }
     current = -1;
+    $body.removeEventListener('click', close, false);
   }
 
   return { init : init };
