34a55e93db290b532fd3763561bd4a2848945987
John Ferris <john@pixel-whip.com>
2014-01-27 22:15:55 -0700

    Remove jQuery dependency on menu.

--- core/scripts/sg-hzmenu.js
+++ core/scripts/sg-hzmenu.js
@@ -1,45 +1,54 @@
 // Based on Codrop's Horizontal drop down menu
 // http://tympanus.net/codrops/2013/03/05/horizontal-drop-down-menu/
-var sgHorizontalMenu = (function($) {
- 
-    var $listItems = $( '#sg-hzmenu > ul > li' ),
-        $menuItems = $listItems.children( 'a' ),
-        $body = $( 'body' ),
-        current = -1;
- 
-    function init() {
-        $menuItems.on( 'click', open );
-        $listItems.on( 'click', function( event ) { event.stopPropagation(); } );
-    }
- 
-    function open( event ) {
- 
-        if( current !== -1 ) {
-            $listItems.eq( current ).removeClass( 'sg-hzopen' );
-        }
- 
-        var $item = $( event.currentTarget ).parent( 'li' ),
-            idx = $item.index();
- 
-        if( current === idx ) {
-            $item.removeClass( 'sg-hzopen' );
-            current = -1;
-        }
-        else {
-            $item.addClass( 'sg-hzopen' );
-            current = idx;
-            $body.off( 'click' ).on( 'click', close );
-        }
- 
-        return false;
- 
-    }
- 
-    function close( event ) {
-        $listItems.eq( current ).removeClass( 'sg-hzopen' );
-        current = -1;
-    }
- 
-    return { init : init };
- 
-})(jQuery);
+var sgHorizontalMenu = (function() {
+
+  var $listItems = document.querySelectorAll('#sg-hzmenu > ul > li'),
+    $body = document.querySelector('body'),
+    current = -1;
+
+  function init() {
+    document.addEventListener('click', function(e) {
+      if ( e.target.matchesSelector('#sg-hzmenu > ul > li > a') ) {
+        open(e);
+      }
+    }, false);
+    //$listItems.on( 'click', function( event ) { event.stopPropagation(); } );
+  }
+
+  function open( event ) {
+
+    // Remove the open class from any that are set.
+    if( current !== -1 ) {
+      $listItems[current].classList.remove( 'sg-hzopen' );
+    }
+
+    var $item = event.target.parentNode,
+      el = $item,
+      idx = 0;
+
+    while (el.previousElementSibling) {
+      idx++;
+      el = el.previousElementSibling;
+    }
+
+    if( current === idx ) {
+      $item.classList.remove( 'sg-hzopen' );
+      current = -1;
+    }
+    else {
+      $item.classList.add( 'sg-hzopen' );
+      current = idx;
+    }
+
+    return false;
+
+  }
+
+  function close( event ) {
+    $listItems[current].classList.remove( 'sg-hzopen' );
+    current = -1;
+  }
+
+  return { init : init };
+
+})();
